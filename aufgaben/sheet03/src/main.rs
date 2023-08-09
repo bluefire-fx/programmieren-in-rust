42f5d8fb93324411c55b480a06be8e42f5bbfc1a
bluefire-fx <f37_jonathan_stoll@gmx.de>
2023-08-09 12:43:57 +0200

    Add solution for task1 in sheet03

--- aufgaben/sheet03/src/main.rs
+++ aufgaben/sheet03/src/main.rs
@@ -1,3 +1,20 @@
+use crate::task1::rule90::next_step;
+
+mod task1;
+mod task2;
+
 fn main() {
-    println!("Hello, world!");
+    task1_main();
+}
+
+fn task1_main() {
+    let life_cycles = 500000;
+    let mut life_line = task1::rule90::read_input();
+    for _ in 0..life_cycles {
+        for &i in &life_line {
+            print!("{}", if i { "██" } else { "  " })
+        }
+        println!();
+        life_line = next_step(&life_line);
+    }
 }
--- aufgaben/sheet03/src/task1/rule90.rs
+++ aufgaben/sheet03/src/task1/rule90.rs
@@ -1,11 +1,7 @@
 //! Task 3.1: Rule 90
 
-fn main() {
-    // TODO: Task 1c)
-}
-
 /// Reads a valid initial configuration for our automaton from the terminal.
-fn read_input() -> Vec<bool> {
+pub fn read_input() -> Vec<bool> {
     // This tries to read a string from the terminal, checks whether it's
     // valid (only contains 1's and 0's). If the user fails to input a correct
     // string, this routine will ask again until the user finally manages to
@@ -37,15 +33,40 @@ fn read_input() -> Vec<bool> {
         buffer.trim().to_string()
     };
 
-    // TODO: Task 1a)
+    // Task 1a)
+    let input_as_vec = {
+        let mut v = Vec::with_capacity(input.len());
+        for c in input.chars() {
+            v.push(c == '1')
+        }
+        v
+    };
+    input_as_vec
 }
 
-// TODO: Task 1b)
+// Task 1b)
+pub fn next_step(old_step: &[bool]) -> Vec<bool> {
+    let mut next_arr: Vec<bool> = Vec::with_capacity(old_step.len());
+    for i in 0..old_step.len() {
+        next_arr.push(if i == 0 {
+            next_cell(&old_step[old_step.len() - 1], &old_step[i + 1])
+        } else if i == old_step.len() - 1 {
+            next_cell(&old_step[i - 1], &old_step[0])
+        } else {
+            next_cell(&old_step[i - 1], &old_step[i + 1])
+        })
+    }
+    next_arr
+}
+
+fn next_cell(left: &bool, right: &bool) -> bool {
+    *left && !*right || !*left && *right
+}
 
 #[test]
 fn rule90_rules() {
     assert_eq!(next_step(&[false, false, false]), vec![false, false, false]);
-    assert_eq!(next_step(&[ true, false, false]), vec![false,  true,  true]);
-    assert_eq!(next_step(&[ true,  true, false]), vec![ true,  true, false]);
-    assert_eq!(next_step(&[ true,  true,  true]), vec![false, false, false]);
+    assert_eq!(next_step(&[true, false, false]), vec![false, true, true]);
+    assert_eq!(next_step(&[true, true, false]), vec![true, true, false]);
+    assert_eq!(next_step(&[true, true, true]), vec![false, false, false]);
 }
